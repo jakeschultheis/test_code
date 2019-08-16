9477e0016b414131e3bed0ba8b1859f9bdb2e47e
Jake Schultheis <ls.jake@gmail.com>
2019-08-15 21:51:01 -0500

    Create aws_micro.tf

---
+++ aws_micro.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+}
+
+resource "aws_instance" "example" {
+
+  ami         = "ami-035b3c7efe6d061d5"
+  instance_type = "t2.micro"
+
+}
